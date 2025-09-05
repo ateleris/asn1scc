521097d4f6bf7dc75900f640786ec662cc14a0b5
Julia Hartmann <julia.hartmann@ateleris.ch>
2025-09-05 16:39:57 +0200

    add more testcases, rename testcases to usefull names

--- asn1python/tests/test_codec_acn.py
+++ asn1python/tests/test_codec_acn.py
@@ -7,7 +7,7 @@ def acn_encoder() -> ACNEncoder:
     acnEncoder = ACNEncoder()
     return acnEncoder
 
-def test_test(acn_encoder: ACNEncoder) -> None:
+def test_enc_dec_int_positive_integer_const_size(acn_encoder: ACNEncoder) -> None:
     input: int = 5
     size_in_bits = 32
     encoded_res = acn_encoder.enc_int_positive_integer_const_size(input, size_in_bits)
@@ -21,3 +21,21 @@ def test_test(acn_encoder: ACNEncoder) -> None:
     print(decoded_res)
     print(f"Input: {input}, decoded {decoded_res.decoded_value}, Passed: {input == decoded_res.decoded_value}")
     assert input, decoded_res.decoded_value
+
+def test_enc_dec_ints_positive_integer_const_size(acn_encoder: ACNEncoder) -> None:
+    input: list[int] = [5, 29, 80]
+    size_in_bits = 16
+    for i in input:
+        encoded_res = acn_encoder.enc_int_positive_integer_const_size(i, size_in_bits)
+        print(encoded_res)
+
+    print(f"Buffer: {acn_encoder.get_bitstream_buffer()}, len {len(acn_encoder.get_bitstream_buffer()) * 8}")
+
+    acn_decoder = ACNDecoder(acn_encoder.get_bitstream_buffer())
+
+    decoded_res = []
+    for _ in range(len(input)):
+        decoded_res.append(acn_decoder.dec_int_positive_integer_const_size(size_in_bits).decoded_value)
+    print(decoded_res)
+    print(f"Input: {input}, decoded {decoded_res}, Passed: {input == decoded_res}")
+    assert input, decoded_res
