a6dc772e628918983832b1f9e150a75c8cab1322
Julia Hartmann <julia.hartmann@ateleris.ch>
2025-09-09 10:20:01 +0200

    Update unittests to be more generic

--- asn1python/tests/test_codec_acn.py
+++ asn1python/tests/test_codec_acn.py
@@ -8,72 +8,61 @@ def acn_encoder() -> ACNEncoder:
     acnEncoder = ACNEncoder()
     return acnEncoder
 
-sizes = [1, 2, 3, (4, 15), 6, (8, 255), 10, 12, (16, 65_535), (32, 4_294_967_295) 48, 64]
+bits = [1, 2, 3, 4, 6, 8, 10, 12, 16, 32, 48, 64]
 
-def test_enc_dec_int_positive_integer_const_size_single_value(acn_encoder: ACNEncoder) -> None:
-    for size in sizes:
-        # TODO: adjust for different bit sizes
-        input: int = random.randint(0, 4_294_967_295)
-        size_in_bits = 32
-        encoded_res = acn_encoder.enc_int_positive_integer_const_size(input, size_in_bits)
+@pytest.mark.parametrize("bit", bits)
+def test_enc_dec_int_positive_integer_const_size_single_value(acn_encoder: ACNEncoder, bit: int) -> None:
+    max_int_size = (2**bit)-1
+    input_number: int = random.randint(1, max_int_size)
 
-        acn_decoder = ACNDecoder(acn_encoder.get_bitstream_buffer())
+    _ = acn_encoder.enc_int_positive_integer_const_size(input_number, bit)
 
-        decoded_res = acn_decoder.dec_int_positive_integer_const_size(size_in_bits)
-        print(f"Input: {input}, decoded {decoded_res.decoded_value}, Passed: {input == decoded_res.decoded_value}")
-        assert input == decoded_res.decoded_value
+    acn_decoder = ACNDecoder(acn_encoder.get_bitstream_buffer())
 
-def test_enc_dec_int_positive_integer_const_size_multiple_values(acn_encoder: ACNEncoder) -> None:
-    numbers = random.randint(3, 10)
-    input: list[int] = []
-    for i in range(numbers):
-        input.append(random.randint(0, 65_535))
-    size_in_bits = 16
-    for i in input:
-        encoded_res = acn_encoder.enc_int_positive_integer_const_size(i, size_in_bits)
+    decoded_res = acn_decoder.dec_int_positive_integer_const_size(bit)
+    print(f"Input: {input_number}, decoded {decoded_res.decoded_value}, Passed: {input_number == decoded_res.decoded_value}")
+    assert input_number == decoded_res.decoded_value
+
+@pytest.mark.parametrize("bit", bits)
+def test_enc_dec_int_positive_integer_const_size_multiple_values(acn_encoder: ACNEncoder, bit: int) -> None:
+    max_int_size = (2 ** bit) - 1
+    input_numbers: list[int] = []
+    for i in range(random.randint(3, 10)):
+        input_numbers.append(random.randint(0, max_int_size))
+    for i in input_numbers:
+        _ = acn_encoder.enc_int_positive_integer_const_size(i, bit)
 
     acn_decoder = ACNDecoder(acn_encoder.get_bitstream_buffer())
 
     decoded_res = []
-    for _ in range(len(input)):
-        decoded_res.append(acn_decoder.dec_int_positive_integer_const_size(size_in_bits).decoded_value)
-    print(f"Input: {input}, decoded {decoded_res}, Passed: {input == decoded_res}")
-    assert input == decoded_res
+    for _ in range(len(input_numbers)):
+        decoded_res.append(acn_decoder.dec_int_positive_integer_const_size(bit).decoded_value)
+    print(f"Input: {input_numbers}, decoded {decoded_res}, Passed: {input_numbers == decoded_res}")
+    assert input_numbers == decoded_res
 
-def test_enc_dec_int_positive_integer_const_size_zero(acn_encoder: ACNEncoder) -> None:
-    input: int = 0
-    size_in_bits = 1
-    encoded_res = acn_encoder.enc_int_positive_integer_const_size(input, size_in_bits)
-    print(encoded_res)
+@pytest.mark.parametrize("bit", bits)
+def test_enc_dec_int_positive_integer_const_size_zero(acn_encoder: ACNEncoder, bit: int) -> None:
+    input_number: int = 0
 
-    print(f"Buffer: {acn_encoder.get_bitstream_buffer()}, len {len(acn_encoder.get_bitstream_buffer()) * 8}")
-
+    _ = acn_encoder.enc_int_positive_integer_const_size(input_number, bit)
     acn_decoder = ACNDecoder(acn_encoder.get_bitstream_buffer())
+    decoded_res = acn_decoder.dec_int_positive_integer_const_size(bit)
+    print(f"Input: {input_number}, decoded {decoded_res.decoded_value}, Passed: {input_number == decoded_res.decoded_value}")
+    assert input_number == decoded_res.decoded_value
 
-    decoded_res = acn_decoder.dec_int_positive_integer_const_size(size_in_bits)
-    print(decoded_res)
+@pytest.mark.parametrize("bit", bits)
+def test_enc_dec_int_positive_integer_const_size_max_value_32(acn_encoder: ACNEncoder, bit: int) -> None:
+    input: int = (2 ** bit) - 1
+
+    _ = acn_encoder.enc_int_positive_integer_const_size(input, bit)
+    acn_decoder = ACNDecoder(acn_encoder.get_bitstream_buffer())
+    decoded_res = acn_decoder.dec_int_positive_integer_const_size(bit)
     print(f"Input: {input}, decoded {decoded_res.decoded_value}, Passed: {input == decoded_res.decoded_value}")
     assert input == decoded_res.decoded_value
 
-def test_enc_dec_int_positive_integer_const_size_max_value_32(acn_encoder: ACNEncoder) -> None:
-    input: int = 4_294_967_295
-    size_in_bits = 32
-    encoded_res = acn_encoder.enc_int_positive_integer_const_size(input, size_in_bits)
-
-    acn_decoder = ACNDecoder(acn_encoder.get_bitstream_buffer())
-
-    decoded_res = acn_decoder.dec_int_positive_integer_const_size(size_in_bits)
-    print(f"Input: {input}, decoded {decoded_res.decoded_value}, Passed: {input == decoded_res.decoded_value}")
-    assert input == decoded_res.decoded_value
-
-def test_enc_dec_int_positive_integer_const_size_exceed_max_value_32(acn_encoder: ACNEncoder) -> None:
-    input: int = 4_294_967_296
-    size_in_bits = 32
-    encoded_res = acn_encoder.enc_int_positive_integer_const_size(input, size_in_bits)
-
+@pytest.mark.parametrize("bit", bits)
+def test_enc_dec_int_positive_integer_const_size_exceed_max_value_32(acn_encoder: ACNEncoder, bit: int) -> None:
+    input_number: int = (2 ** bit)
+    encoded_res = acn_encoder.enc_int_positive_integer_const_size(input_number, bit)
     assert not encoded_res.success
-    # acn_decoder = ACNDecoder(acn_encoder.get_bitstream_buffer())
-    #
-    # decoded_res = acn_decoder.dec_int_positive_integer_const_size(size_in_bits)
-    # print(f"Input: {input}, decoded {decoded_res.decoded_value}, Passed: {input == decoded_res.decoded_value}")
-    # assert input == decoded_res.decoded_value
+
