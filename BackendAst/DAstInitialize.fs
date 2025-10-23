2ab4039844fca0f20604aaa5d9bc472de751cd8b
Manuel Stutz <manuel.stutz@ateleris.ch>
2025-10-23 09:24:46 +0200

    Fix for other languages

--- BackendAst/DAstInitialize.fs
+++ BackendAst/DAstInitialize.fs
@@ -698,7 +698,7 @@ let createEnumeratedInitFunc (r: Asn1AcnAst.AstRoot) (lm: LanguageMacros) (t: As
                 AutomaticTestCase.initTestCaseFunc =
                     (fun (p:CodegenScope) ->
                         let resVar = p.accessPath.asIdentifier
-                        {InitFunctionResult.funcBody = initEnumerated (lm.lg.getValue p.accessPath) (lm.lg.getNamedItemBackendName (Some typeDefinition) vl) (typeDefinition.longTypedefName2 false) p.accessPath.isOptional resVar; resultVar = resVar; localVariables=[]});
+                        {InitFunctionResult.funcBody = initEnumerated (lm.lg.getValue p.accessPath) (lm.lg.getNamedItemBackendName (Some typeDefinition) vl) (typeDefinition.longTypedefName2 (if ProgrammingLanguage.ActiveLanguages.Head = Python then false else lm.lg.hasModules)) p.accessPath.isOptional resVar; resultVar = resVar; localVariables=[]});
                 testCaseTypeIDsMap = Map.ofList [(t.id, (TcvEnumeratedValue vl.Name.Value))]
             })
     let constantInitExpression () = lm.lg.getNamedItemBackendName (Some typeDefinition) o.items.Head
