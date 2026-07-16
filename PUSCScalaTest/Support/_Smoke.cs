e77493c5be732159279afa80ffea89b473f06f9c
Manuel Stutz <manuel.stutz@ateleris.ch>
2026-07-16 10:03:36 +0200

    Add interop comparison helpers to BuildCache

--- PUSCScalaTest/Support/_Smoke.cs
+++ PUSCScalaTest/Support/_Smoke.cs
@@ -10,4 +10,13 @@ public class _Smoke
         Assert.IsTrue(r.TestsPassed, "Python S1 UPER enc/dec should pass");
         Assert.IsTrue(Directory.GetFiles(r.DatDir, "*.dat").Length > 0, "expected .dat artifacts");
     }
+
+    [TestMethod]
+    public void C_vs_Python_S1_UPER_interop()
+    {
+        var c = BuildCache.EnsureBuilt(new BuildKey(PUS_C_Service.S1, Lang.C, Enc.UPER));
+        Assert.IsTrue(c.TestsPassed, "C S1 UPER enc/dec should pass");
+        var py = BuildCache.EnsureBuilt(new BuildKey(PUS_C_Service.S1, Lang.Python, Enc.UPER));
+        BuildCache.CompareDat(c, py, BuildCache.Exclusions(Lang.Python, PUS_C_Service.S1));
+    }
 }
